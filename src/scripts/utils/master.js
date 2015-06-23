8a6bf1267e14d3b78fbfcf7cdfea720d1a1d5733
Greg Babula <gbabula@gmail.com>
2015-06-22 20:27:53 -0400

    updated utils

--- src/scripts/utils/master.js
+++ src/scripts/utils/master.js
@@ -7,57 +7,26 @@
 
 'use strict';
 
-let months = [
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'May',
-    'Jun',
-    'Jul',
-    'Aug',
-    'Sep',
-    'Oct',
-    'Nov',
-    'Dec'
-];
-
-/**
- *
- * @function pad
- * @param {Number} n
- * @returns {String} padded number
- *
- */
-function pad(n) {
-    return n < 10 ? '0' + n.toString(10) : n.toString(10);
-}
-
-/**
- *
- * @function timestamp
- * @returns {String} timestamp (i.e. 26 Feb 16:19:34)
- *
- */
-function timestamp() {
-
-    let d = new Date();
-    let time = [
-        pad(d.getHours()),
-        pad(d.getMinutes()),
-        pad(d.getSeconds())
-    ].join(':');
-
-    return [d.getDate(), months[d.getMonth()], time].join(' ');
-
-}
-
 /**
  *
  * @name utils
  *
  */
 let utils = {
+    months: [
+        'Jan',
+        'Feb',
+        'Mar',
+        'Apr',
+        'May',
+        'Jun',
+        'Jul',
+        'Aug',
+        'Sep',
+        'Oct',
+        'Nov',
+        'Dec'
+    ],
     /**
      *
      * @method log
@@ -66,11 +35,45 @@ let utils = {
      */
     log() {
 
+        let timestamp = utils.timestamp;
         let args = Array.prototype.slice.call(arguments);
 
         args.unshift(timestamp() + ' - g5-component :');
 
         console.log.apply(console, args);
+
+    },
+    /**
+     *
+     * @method pad
+     * @param {Number} n
+     * @returns {String} padded number
+     *
+     */
+    pad(n) {
+
+        return n < 10 ? '0' + n.toString(10) : n.toString(10);
+
+    },
+    /**
+     *
+     * @method timestamp
+     * @returns {String} timestamp (i.e. 26 Feb 16:19:34)
+     *
+     */
+    timestamp() {
+
+        let pad = utils.pad;
+        let months = utils.months;
+        let d = new Date();
+
+        let time = [
+            pad(d.getHours()),
+            pad(d.getMinutes()),
+            pad(d.getSeconds())
+        ].join(':');
+
+        return [d.getDate(), months[d.getMonth()], time].join(' ');
 
     }
 };
