6efca8cf38104c44bde98d5c9bfb8e47a4942bae
Greg Babula <greg.babula@mlb.com>
2015-08-28 11:00:51 -0400

    adjustments

--- src/scripts/component/master.js
+++ src/scripts/component/master.js
@@ -70,6 +70,7 @@ let component = {
          *
          * @event click
          * @param {Object} e event
+         * @description simple event example
          *
          */
         this.$element.on('click', 'dt', function(e) {
