d04c6efcd0a2e720f73bce4cc0284bc97abb0810
Greg Babula <gbabula@gmail.com>
2015-11-04 00:03:46 -0500

    added helpers

--- src/scripts/component/helpers.js
+++ src/scripts/component/helpers.js
@@ -19,10 +19,50 @@ let helpers = {
      *
      * @method upcase
      * @description example helper, transforms text to uppercase
+     * @param {String} s
+     * @returns {String} lowercase String
      *
      */
     'upcase': function(s) {
+
         return s.toUpperCase();
+
+    },
+    /**
+     *
+     * @function ifOr
+     * @param {Boolean} a
+     * @param {Boolean} b
+     * @param {Object} options
+     * @returns {Function}
+     *
+     */
+    'ifOr': function(a, b, options) {
+
+        if (a || b) {
+            return options.fn(this);
+        } else {
+            return options.inverse(this);
+        }
+
+    },
+    /**
+     *
+     * @function ifAnd
+     * @param {Boolean} a
+     * @param {Boolean} b
+     * @param {Object} options
+     * @returns {Function}
+     *
+     */
+    'ifAnd': function(a, b, options) {
+
+        if (a && b) {
+            return options.fn(this);
+        } else {
+            return options.inverse(this);
+        }
+
     }
 };
 
