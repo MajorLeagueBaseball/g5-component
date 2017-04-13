14cb02de2a4f9a0fab2391f99fe98cd9cd0b9136
gefu <George.Fu@mlb.com>
2017-04-13 10:38:17 -0400

    annotate example composition

--- example/src/g5-component-composition.js
+++ example/src/g5-component-composition.js
@@ -7,23 +7,67 @@
  *
  * Here we will create a List component which contains Row components.
  *
- * @todo annotate with comments?
- *
  */
 
 import G5Component from './../../src/scripts/g5-component';
 import { inject } from './../../src/scripts/dependencies/defaultInjector';
 
+/**
+ *
+ * Suppose you have the need for a structure like the following:
+ * <ul>
+ *     <li></li>
+ *     <li></li>
+ *     ...
+ * </ul>
+ *
+ * As in
+ *
+ * <scoreboard>
+ *     <game></game>
+ *     <game></game>
+ *     ...
+ * </scoreboard>
+ *
+ * This can be a single component, of course, but it could also be a composition if you would like the individual
+ * parts <li> to have independent behaviors such as managing their own event listeners, data, or polling.
+ *
+ * Those can be accomplished in a monolithic component as well, but there is an additional benefit of re-usability
+ * which a monolith does not provide.
+ *
+ */
+
 class Row extends G5Component {
 
     constructor(opts) {
 
+        /**
+         * Only stub functionality as baseline.
+         * @type {Object}
+         */
         const implementation = G5Component.dependencies;
 
+        /**
+         *
+         * For example's expedience, our Row component will accept data through its constructor: opts.
+         * @param {object} data
+         * @param {object} opts
+         * @returns {object}
+         *
+         */
         implementation.extender = function (data, opts) {
             return opts.row;
         };
 
+        /**
+         *
+         * Also for expedience, our template will directly return an HTML element instead of
+         * using a Handlebars template as would normally be the case.
+         *
+         * @param {Object} data
+         * @returns {HTMLElement} (implementations of [template] can also return a string)
+         *
+         */
         implementation.template = function (data) {
 
             const li = document.createElement('li');
@@ -40,6 +84,12 @@ class Row extends G5Component {
 
     }
 
+    /**
+     *
+     * @returns {HTMLElement|String}
+     * @desc Offer a top level function for the parent component to extract the template.
+     *
+     */
     template() {
         return this.implementations.template(
             this.implementations.extender(null, this.opts)
@@ -52,17 +102,37 @@ class List extends G5Component {
 
     constructor(opts) {
 
+        /**
+         * For the List, its implementation uses the G5Component baseline instead of the stubs like with Row.
+         * @type {Object}
+         */
         const implementation = inject({});
 
+        /**
+         *
+         * Nothing to do for our simple data extender.
+         * @param {object} data
+         * @param {object} opts
+         * @returns {object}
+         *
+         */
         implementation.extender = function (data, opts) {
-            return opts.rows;
+            return data;
         };
 
+        /**
+         *
+         * Again demonstrating a direct template function in lieu of a hbsfy-compiled function.
+         *
+         * @param {Object} data
+         * @returns {HTMLElement} (implementations of [template] can also return a string)
+         *
+         */
         implementation.template = function (data) {
 
             const list = document.createElement('ul');
 
-            for (const row of data) { // note; Symbol not for production
+            for (const row of data) { // note: Symbol not for production
 
                 list.appendChild(new Row({ row }).template());
 
@@ -78,6 +148,15 @@ class List extends G5Component {
 
 }
 
+/**
+ *
+ * @param {object} opts
+ * @returns {List}
+ *
+ * Exporting the traditional component factory at the top level.
+ * @see ./../composition.html in examples folder.
+ *
+ */
 module.exports = function (opts) {
 
     return new List(opts);
